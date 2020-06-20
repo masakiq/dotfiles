feb406d06407c404260afe28df82aabbf5cc2f01
maeda1150 <>
2020-06-20 13:25:05 +0900

    Add tmux/.tmux.conf
    modified:   install.sh
    new file:   tmux/.tmux.conf

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s `pwd`/vim/.vimrc ~/.vimrc
 ln -s `pwd`/vim/.vimrc ~/.config/nvim/init.vim
 ln -s `pwd`/vim/.vimrc ~/.ideavimrc
 ln -s `pwd`/tig/.tigrc ~/.tigrc
+ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
