3275e0ba94685307a65bda7a4318f4180c3c104f
maeda1150 <>
2020-06-27 16:17:03 +0900

    Add vscode settings
    modified:   install.sh
    new file:   vscode/keybindings.json
    new file:   vscode/settings.json

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -s `pwd`/vim/.vimrc ~/.config/nvim/init.vim
 ln -s `pwd`/vim/.vimrc ~/.ideavimrc
 ln -s `pwd`/tig/.tigrc ~/.tigrc
 ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s `pwd`/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
