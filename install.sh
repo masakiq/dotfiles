b9a2d655cf1747b9d3c8f01c287ab2e94e6d28e6
maeda1150 <>
2020-10-04 23:01:40 +0900

    Setup alacritty
    new file:   alacritty/.alacritty.yml
    modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s `pwd`/vim/functions/normal ~/.vim/functions/normal
 ln -s `pwd`/vim/functions/visual ~/.vim/functions/visual
 ln -s `pwd`/tig/.tigrc ~/.tigrc
 ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/alacritty/.alacritty.yml ~/.alacritty.yml
 ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
 ln -s `pwd`/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
