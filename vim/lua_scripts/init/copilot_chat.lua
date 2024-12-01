a35132496326811e0ddef6ddc7514729bb036cb2
masakiq <>
2024-12-01 15:53:37 +0900

    feat: update model to gpt-4o-mini in CopilotChat
    Updated the model used in CopilotChat from 'o1-mini' to
    'gpt-4o-mini' for improved performance and capabilities.
    
    modified:   vim/lua_scripts/init/copilot_chat.lua

--- vim/lua_scripts/init/copilot_chat.lua
+++ vim/lua_scripts/init/copilot_chat.lua
@@ -11,7 +11,7 @@ local custom_additional_prompt = 'And, please translate it into Japanese as well
 
 require('CopilotChat').setup {
   debug = true, -- Enable debugging
-  model = 'o1-mini',
+  model = 'gpt-4o-mini',
   highlight_selection = false,
 
   prompts = {
