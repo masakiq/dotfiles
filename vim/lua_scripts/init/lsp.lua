49fca63d6252840caafbdbbdebfa0ca99387474d
masakiq <4811459+masakiq@users.noreply.github.com>
2025-05-04 17:16:35 +0900

    feat: add markdown-oxide LSP support
    This commit introduces support for the markdown-oxide language server
    for Markdown files. An autocommand is created to start the language
    server when a Markdown file is opened, allowing for enhanced editing
    features and capabilities.
    
    modified:   vim/lua_scripts/init/lsp.lua

--- vim/lua_scripts/init/lsp.lua
+++ vim/lua_scripts/init/lsp.lua
@@ -4,6 +4,21 @@ require("config-local").setup({
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "md" },
+  callback = function()
+    vim.lsp.start({
+      name = "markdown-oxide",
+      cmd = { "markdown-oxide" },
+      root_dir = vim.fs.dirname(vim.fs.find({ ".git", "README.md" }, { upward = true })[1]),
+      settings = {
+        -- Optional: customize settings
+        -- These would correspond to Markdown Oxide's configuration options
+      },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
   pattern = { "json", "lua", "yaml", "html", "markdown", "sql" },
   callback = function()
     vim.lsp.start({
