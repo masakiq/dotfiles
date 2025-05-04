c968e5683a3079da3b70ef22770ad96909f5e4a9
masakiq <4811459+masakiq@users.noreply.github.com>
2025-05-04 17:22:55 +0900

    feat(lsp): add support for TOML files with taplo LSP
    This commit introduces an autocommand for TOML file types that starts
    the taplo language server. The server is configured to find the root
    directory based on the presence of a .git directory or a .toml file.
    
    modified:   vim/lua_scripts/init/lsp.lua

--- vim/lua_scripts/init/lsp.lua
+++ vim/lua_scripts/init/lsp.lua
@@ -19,6 +19,18 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "toml" },
+  callback = function()
+    vim.lsp.start({
+      name = "taplo",
+      cmd = { "taplo", "lsp", "stdio" },
+      root_dir = vim.fs.dirname(vim.fs.find({ ".git", "*.toml" }, { upward = true })[1]),
+      settings = {},
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
   pattern = { "json", "lua", "yaml", "html", "markdown", "sql" },
   callback = function()
     vim.lsp.start({
