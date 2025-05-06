2df317dc6d915a981a011683154d1da9041c3d70
masakiq <4811459+masakiq@users.noreply.github.com>
2025-05-06 17:41:08 +0900

    feat(vim): add lazy.nvim plugin manager
    modified:   vim/init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -7,3 +7,19 @@ vim.o.exrc = true
 
 -- .nvim.lua 内の外部プログラム呼び出しを禁止する
 vim.o.secure = true
+
+-- データディレクトリ内に lazy.nvim がなければクローン
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+-- ランタイムパスに追加
+vim.opt.rtp:prepend(lazypath)
