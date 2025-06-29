74baf87b4e70cd0c92aecea8d69660f78ec244a6
masakiq <4811459+masakiq@users.noreply.github.com>
2025-06-29 23:08:08 +0900

    feat(vim): oil.nvim add custom keymaps for file navigation
    - Add split/vsplit keymaps (<C-s>, <C-v>)
    - Add <C-e> for standard selection
    - Override <Enter> to open files in new tab instead of buffer
    - Files open via 'tab drop', directories use default behavior
    - Oil window closes automatically when opening files
    
    modified:   vim/plugin/oil_nvim.lua

--- vim/plugin/oil_nvim.lua
+++ vim/plugin/oil_nvim.lua
@@ -10,6 +10,28 @@ require("oil").setup({
   view_options = {
     show_hidden = true,
   },
+  keymaps = {
+    ["<C-s>"] = "actions.select_split",
+    ["<C-v>"] = "actions.select_vsplit",
+    ["<C-e>"] = "actions.select",
+    ["<Enter>"] = {
+      callback = function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if entry then
+          local dir = oil.get_current_dir()
+          local path = dir .. entry.name
+          local stat = vim.loop.fs_stat(path)
+          if stat and stat.type == "file" then
+            oil.close()
+            vim.cmd("tab drop " .. vim.fn.fnameescape(path))
+          else
+            oil.select()
+          end
+        end
+      end,
+    },
+  },
 })
 
 vim.api.nvim_create_augroup("OilRelPathFix", {})
