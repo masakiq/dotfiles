e3690953e4df7b8c72f28d6de4c372eabc13cc96
masakiq <4811459+masakiq@users.noreply.github.com>
2025-06-29 22:43:06 +0900

    feat(telescope): replace tab select with tab drop action
    - Add custom tab_drop_action function using vim's tab drop
    - Replace <C-t> with <C-e> for select_default action
    - Map <Enter> to tab_drop_action in both insert and normal modes
    - Use action_state to get selected entry path/filename/value
    
    This changes the default behavior from opening files in new tabs
    to using tab drop, which reuses existing tabs for already open
    files, providing better tab management.
    
    modified:   vim/plugin/telescope.lua

--- vim/plugin/telescope.lua
+++ vim/plugin/telescope.lua
@@ -1,16 +1,32 @@
 local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local tab_drop_action = function(prompt_bufnr)
+  local selection = action_state.get_selected_entry()
+  actions.close(prompt_bufnr)
+
+  if selection then
+    local path = selection.path or selection.filename or selection.value
+    if path then
+      vim.cmd("tab drop " .. path)
+    end
+  end
+end
+
 require("telescope").setup({
   defaults = {
     mappings = {
       i = {
         ["<C-s>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+        ["<C-e>"] = actions.select_default,
+        ["<Enter>"] = tab_drop_action,
       },
       n = {
         ["<C-s>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+        ["<C-e>"] = actions.select_default,
+        ["<Enter>"] = tab_drop_action,
       },
     },
   },
